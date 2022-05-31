cad21932d5a7dca8eaae351337eb1a0bed1959e5
SargasTM <serjko87@gmail.com>
2022-05-31 15:32:09 +0300

    SYS: version up

--- jet-grid-builder.php
+++ jet-grid-builder.php
@@ -3,7 +3,7 @@
  * Plugin Name: JetGridBuilder
  * Description: Addon for creating wow-grids on your website
  * Plugin URI:  https://crocoblock.com/plugins/jetgridbuilder/
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Crocoblock
  * Author URI:  https://crocoblock.com/
  * Text Domain: jet-grid-builder
@@ -17,7 +17,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die();
 }
 
-define( 'JET_GRID_BUILDER_VERSION', '1.1.1' );
+define( 'JET_GRID_BUILDER_VERSION', '1.1.2' );
 define( 'JET_GRID_BUILDER__FILE__', __FILE__ );
 define( 'JET_GRID_BUILDER_PLUGIN_BASE', plugin_basename( JET_GRID_BUILDER__FILE__ ) );
 define( 'JET_GRID_BUILDER_PATH', plugin_dir_path( JET_GRID_BUILDER__FILE__ ) );
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: posts grid, custom post grid, grid, masonry layout, justify layout, elemen
 Requires at least: 5.4
 Tested up to: 5.8.2
 Requires PHP: 7.2
-Stable tag: 1.1.1
+Stable tag: 1.1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
  
